0f5fd72df682de8dcb72385f1f1eaff1b83bbe23
Vikas Singh <vikashverma3001@gmail.com>
2025-07-30 19:35:47 +0530

    understand how to create state variable in CBM

--- coding-advanced/src/components/User.js
+++ coding-advanced/src/components/User.js
@@ -1,12 +1,19 @@
+import { useState } from "react"
+
 const User = ({
   name,
   location
 })=> {
 
+  const [count] = useState(0);
+  const [count2] = useState(1);
+
   return (
     <div>
       <h1>Name : {name}</h1>
-      <h4>Location: {location} </h4>
+      <h2>Location: {location} </h2>
+      <h4>Count 1 : {count}</h4>
+      <h4>Count 2 : {count2}</h4>
     </div>
   )
 }
--- coding-advanced/src/components/class/UserCalss.js
+++ coding-advanced/src/components/class/UserCalss.js
@@ -1,21 +1,26 @@
-import React from 'react'
+import React from "react";
 
-class UserClass extends React.Component{
-  constructor(props){
+class UserClass extends React.Component {
+  constructor(props) {
     super(props);
-  }
-  render(){
 
-    const {
-      name,
-      location
-    } = this.props
+    this.state = {
+      count: 1,
+      count2: 2,
+    };
+  }
+  render() {
+    const { count, count2 } = this.state;
+
+    const { name, location } = this.props;
     return (
       <div>
-      <h1>Name: {name}</h1>
-      <h4>Location: {location}</h4>
-    </div>
-    )
+        <h1>Name: {name}</h1>
+        <h4>Location: {location}</h4>
+        <h4>Class Count 1 : {count}</h4>
+        <h4>Class Count 2 : {count2}</h4>
+      </div>
+    );
   }
 }
 
