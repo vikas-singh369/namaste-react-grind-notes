f78fc78e3b007b1a11a8a6d392ce42b54ae36170
Vikas Singh <vikashverma3001@gmail.com>
2025-07-31 22:39:58 +0530

    feat: refactor UserClass to improve logging and fetch user data from API

--- coding-advanced/src/components/About.js
+++ coding-advanced/src/components/About.js
@@ -1,24 +1,24 @@
 import UserClass from "./class/UserCalss";
 import User from "./User";
+import React from "react";
 
 class About extends React.Component {
-
-  constructor (){
-    console.log("parent constructor")
+  constructor() {
+    super();
+    console.log("parent constructor");
   }
-  componentDidMount(){
+  componentDidMount() {
     console.log("parent did mount called");
   }
 
-
   render() {
-    console.log("Parent Render")
+    console.log("Parent Render");
     return (
       <div>
         <div>
           <h1>About</h1>
           <p>This is a About Page</p>
-          <UserClass name={"Vikas (Class-Component)"} location={"Hydrabad"} />
+          <UserClass name={"first child"} location={"Hydrabad"} />
         </div>
       </div>
     );
--- coding-advanced/src/components/class/UserCalss.js
+++ coding-advanced/src/components/class/UserCalss.js
@@ -3,56 +3,56 @@ import React from "react";
 // Creating a class-based component that extends React.Component
 class UserClass extends React.Component {
   constructor(props) {
-    console.log("child counstructor");
-
     super(props); // Must call super(props) to use `this.props` in constructor
+
+    console.log(this.props.name + "child counstructor");
 
     // Initializing component's state (like internal data)
     this.state = {
-      count: 1,
-      count2: 2,
+      userInfo:{
+        name: "ram",
+        location: ""
+      }
     };
   }
 
-  componentDidMount(){
-    console.log("Child componennt did mount")
+  async componentDidMount(){
+    console.log(this.props.name + "Child componennt did mount")
+
+const  data = await fetch(" https://api.github.com/users/vikas-singh369");
+
+const json = await data.json();
+
+this.setState({
+  userInfo:json
+});
+
+  }
+
+  componentDidUpdate(){
+    console.log( this.props.name + "Child component update")
+  }
+
+  componentWillUnmount(){
+    console.log( this.props.name + "Child component unmount")
   }
 
   // render() is a required method in class components — it returns JSX
   render() {
-    console.log("Child render called");
+    console.log(this.props.name + "Child render called");
 
-    const { count, count2 } = this.state;
+    // const { name, location } = this.props; now not taking data from props using api to learn
 
-    const { name, location } = this.props;
 
+
+    const {
+      name,
+      location
+    } = this.state.userInfo;
     return (
       <div>
         <h1>Name: {name}</h1>
         <h4>Location: {location}</h4>
-        <h4>Class Count 1 : {count}</h4>
-        <h4>Class Count 2 : {count2}</h4>
-        <button 
-          onClick={() => {
-            this.setState({
-              count: this.state.count + 1, //  Only 'count' gets updated, 'count2' stays the same
-            });
-          }}
-        >
-          Increse
-        </button>
-
-        <button 
-          onClick={() => {
-            this.setState({
-              count2: this.state.count2 + 1,
-            });
-          }}
-        >
-          Increse count2 
-        </button>
-
-
       </div>
     );
   }
