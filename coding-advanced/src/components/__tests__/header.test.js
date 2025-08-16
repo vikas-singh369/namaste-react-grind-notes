cc8d743cb5987b31241974bddbaf269000f26945
Vikas Singh <vikashverma3001@gmail.com>
2025-08-16 19:49:36 +0530

    Add: learn fireEvent and testing using different different approaches.

--- coding-advanced/src/components/__tests__/header.test.js
+++ coding-advanced/src/components/__tests__/header.test.js
@@ -1,10 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Header from "../Header";
 import { BrowserRouter } from "react-router";
 import { Provider } from "react-redux";
 import appStore from "../../redux/appStore";
+import "@testing-library/jest-dom"
 
-test("should render header component with login button", () => {
+it("should render header component with login button", () => {
   render(
     <BrowserRouter>
       <Provider store={appStore}>
@@ -13,6 +14,40 @@ test("should render header component with login button", () => {
     </BrowserRouter>
   );
 
-  // const loginButton = screen.getByRole("button", { name: /login/i });
-  // expect(loginButton).toBeInTheDocument();
+  const loginButton = screen.getByRole("button", { name: /login/i });
+  expect(loginButton).toBeInTheDocument();
+});
+
+
+// check if click so logout show or not
+it("should render header component with login button", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginButton = screen.getByRole("button", { name: /login/i });
+
+  fireEvent.click(loginButton)
+
+  const logoutButton = screen.getByRole("button", { name: /logout/i });
+  expect(logoutButton).toBeInTheDocument();
+});
+
+
+it("should render header component with login button", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const cartItemText = screen.getByText(/Cart/i);
+
+  expect(cartItemText).toBeInTheDocument();
 });
