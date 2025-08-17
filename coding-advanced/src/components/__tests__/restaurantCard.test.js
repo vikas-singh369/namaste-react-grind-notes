607702d0a73c5c9d12c936387f604b530ddae751
Vikas Singh <vikashverma3001@gmail.com>
2025-08-17 13:14:11 +0530

    unit testing learn with cliarity practice and done

---
+++ coding-advanced/src/components/__tests__/restaurantCard.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import RestaurantCard from "../RestaurantCard";
+import { isVegReastaurant } from "../RestaurantCard";
+import MOCK_DATA from "./mock/mockData.json";
+import "@testing-library/jest-dom";
+
+const VegReastaurantCard = isVegReastaurant(RestaurantCard);
+
+it("shoud render restaurant card", () => {
+  render(<RestaurantCard resData={MOCK_DATA.info} />);
+
+  const name = screen.getByText("Theobroma");
+
+  expect(name).toBeInTheDocument();
+});
+
+it("shoud render restaurant card with veg label or not", () => {
+  render(<VegReastaurantCard resData={MOCK_DATA.info} />);
+
+  const name = screen.getByText("Theobroma");
+
+  expect(name).toBeInTheDocument();
+});
