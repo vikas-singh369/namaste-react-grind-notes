2bc8db22adc9b14f6b30e7d5db649ffd78e7a1e6
Vikas Singh <vikashverma3001@gmail.com>
2025-08-23 01:18:08 +0530

    refactor : design

--- coding-advanced/src/components/Header.js
+++ coding-advanced/src/components/Header.js
@@ -6,32 +6,32 @@ import { useSelector } from "react-redux";
 import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
-  const [btnName, setBtName] = useState("login");
+  const [btnName, setBtName] = useState("Login");
   const cartItems = useSelector((store) => store.cart.items);
   const { logedInUser } = useContext(UserContext);
   const onlineStatus = useOnlineStatus();
 
   return (
-    <header className=" bg-amber-600/75 flex justify-between items-center">
+    <header className=" bg-amber-500/70 flex justify-between items-center">
       <img data-testid="logo" className="w-30" src={LOGO_URL} />
 
       <ul className="flex items-center justify-center">
-        <li className="px-2.5 font-bold"><Link to="/">Home</Link></li>
-        <li className="px-2.5 font-bold"><Link to="/about">About</Link></li>
-        <li className="px-2.5 font-bold"><Link to="/contact">Contact</Link></li>
-        <li className="px-2.5 font-bold"><Link to="/cart">Cart - {cartItems.length}</Link></li>
+        <li className=" font-bold p-2.5 hover:bg-amber-500/95 rounded-lg "><Link to="/">Home</Link></li>
+        <li className="font-bold p-2.5 hover:bg-amber-500/95 rounded-lg"><Link to="/about">About</Link></li>
+        <li className="font-bold p-2.5 hover:bg-amber-500/95 rounded-lg"><Link to="/contact">Contact</Link></li>
+        <li className="font-bold p-2.5 hover:bg-amber-500/95 rounded-lg"><Link to="/cart">🛒 - {cartItems.length}</Link></li>
 
-        <li><Link to="/login" className="px-2.5 font-bold">
+        <li><Link to="/login" className="font-bold p-2.5 hover:bg-amber-500/95 rounded-lg">
           <button
             onClick={() => {
-              btnName === "login" ? setBtName("logout") : setBtName("login");
+              btnName === "Login" ? setBtName("Logout") : setBtName("Login");
             }}
           >
             {btnName}
           </button>
           </Link>
         </li>
-        <li className="px-2.5 font-bold"><span>{onlineStatus ? "🟢" : "🔴"}</span> {logedInUser}</li>
+        <li className="font-bold p-2.5 hover:bg-amber-500/95 rounded-lg"><span>{onlineStatus ? "🟢" : "🔴"}</span> {logedInUser}</li>
       </ul>
     </header>
   );
